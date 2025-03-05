aaa2e490705da56600094c0a81f6d77d8ec5ac94
Leo Lu <lyuholu@gmail.com>
2025-03-05 16:13:50 -0500

    feat: show email in profile page & implement sign-out

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,30 @@
+'use client';
+
+import { useEmail } from '@/context/Context';
+import { supabase } from '@/lib/utils';
+import { useRouter } from 'next/navigation';
+
 export default function Profile() {
-    return <>profile</>;
+    const email = useEmail();
+    const router = useRouter();
+
+    async function signOut() {
+        const { error } = await supabase.auth.signOut();
+
+        if (error) {
+            alert('Error!');
+            console.error(error);
+        } else {
+            router.push('/');
+        }
+    }
+
+    return (
+        <>
+            <main>
+                <h1>👋 {email}</h1>
+                <button onClick={signOut}>Sign out</button>
+            </main>
+        </>
+    );
 }
