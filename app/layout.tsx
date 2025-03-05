0daa75a613c9cd43ff9c6edaa352b7f147c6b60d
Leo Lu <lyuholu@gmail.com>
2025-03-05 13:58:18 -0500

    fix: add children

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,40 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { Geist } from "next/font/google";
-import { ThemeProvider } from "next-themes";
-import Link from "next/link";
-import "./globals.css";
+import DeployButton from '@/components/deploy-button';
+import { EnvVarWarning } from '@/components/env-var-warning';
+import HeaderAuth from '@/components/header-auth';
+import { ThemeSwitcher } from '@/components/theme-switcher';
+import { hasEnvVars } from '@/utils/supabase/check-env-vars';
+import { Geist } from 'next/font/google';
+import { ThemeProvider } from 'next-themes';
+import Link from 'next/link';
+import './globals.css';
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000';
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Sportify",
-  description: "",
+    metadataBase: new URL(defaultUrl),
+    title: 'Sportify',
+    description: '',
 };
 
 const geistSans = Geist({
-  display: "swap",
-  subsets: ["latin"],
+    display: 'swap',
+    subsets: ['latin'],
 });
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-      </body>
-    </html>
-  );
+    return (
+        <html
+            lang="en"
+            className={geistSans.className}
+            suppressHydrationWarning
+        >
+            <body className="bg-background text-foreground">{children}</body>
+        </html>
+    );
 }
