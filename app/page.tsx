b5bf12b91d673116a687e1d04c2f759ffcfcb51e
Leo Lu <lyuholu@gmail.com>
2025-03-05 14:34:30 -0500

    feat: allow users to sign in

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import ContinueWithGoogleBtn from '@/app/ContinueWithGoogleBtn';
 
-export default async function Welcome() {
+export default function Welcome() {
     return (
         <>
             <main className="min-h-screen bg-sky-500 flex flex-col justify-center items-center">
-                <h1 className="text-6xl text-white font-bold mb-12">Sportify</h1>
+                <h1 className="text-6xl text-white font-bold mb-12">
+                    Sportify
+                </h1>
                 <ContinueWithGoogleBtn />
             </main>
         </>
