9ac3104c93aaa4806cbe23de6ade33f80cee24d7
Leo Lu <lyuholu@gmail.com>
2025-03-06 01:56:29 -0500

    feat: add sports to /new

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,3 +1,26 @@
 export default function New() {
-    return <></>;
+    return (
+        <main className="p-4 min-h-screen">
+            <h1 className="text-center pb-4 border-b-black border-b-2 text-2xl font-bold">
+                揪運動
+            </h1>
+            <section className="flex flex-wrap justify-between gap-4 mt-4">
+                <button className="w-36 h-36 rounded-xl cursor-pointer flex justify-center items-center border-black border-4 bg-white text-8xl">
+                    ⚽
+                </button>
+                <button className="w-36 h-36 rounded-xl cursor-pointer flex justify-center items-center border-black border-4 bg-white text-8xl">
+                    🏀
+                </button>
+                <button className="w-36 h-36 rounded-xl cursor-pointer flex justify-center items-center border-black border-4 bg-white text-8xl">
+                    🎾
+                </button>
+                <button className="w-36 h-36 rounded-xl cursor-pointer flex justify-center items-center border-black border-4 bg-white text-8xl">
+                    🏓
+                </button>
+                <button className="w-36 h-36 rounded-xl cursor-pointer flex justify-center items-center border-black border-4 bg-white text-8xl">
+                    🏸
+                </button>
+            </section>
+        </main>
+    );
 }
