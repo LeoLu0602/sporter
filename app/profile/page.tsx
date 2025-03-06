e71d8c7cbe3e401549f3ed7e0bbc7a81c2580b21
Leo Lu <lyuholu@gmail.com>
2025-03-06 16:02:52 -0500

    fix: add one more level

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -171,16 +171,18 @@ export default function Profile() {
     function explainLevel(level: number): string {
         switch (level) {
             case 0:
-                return '新手';
+                return '沒興趣';
             case 1:
-                return '休閒';
+                return '新手';
             case 2:
-                return '中階';
+                return '休閒';
             case 3:
-                return '進階';
+                return '中階';
             case 4:
-                return '半職業';
+                return '進階';
             case 5:
+                return '半職業';
+            case 6:
                 return '職業';
         }
 
--- components/LevelBar.tsx
+++ components/LevelBar.tsx
@@ -11,7 +11,7 @@ export default function LevelBar({
         <section className="relative">
             <span className="absolute left-0 top-[38px] w-full h-1 bg-black" />
             <section className="w-full flex justify-between items-center py-8 relative z-10">
-                {[0, 1, 2, 3, 4, 5].map((i) => (
+                {[0, 1, 2, 3, 4, 5, 6].map((i) => (
                     <Level
                         key={i}
                         index={i}
