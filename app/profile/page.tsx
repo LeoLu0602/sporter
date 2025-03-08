148f9e983e25b1ebc8aa87d37e3f518e226c6906
Leo Lu <lyuholu@gmail.com>
2025-03-08 17:25:35 -0500

    refactor: move explainLevel to utils

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,6 +6,7 @@ import clsx from 'clsx';
 import { useRouter } from 'next/navigation';
 import { ChangeEvent, useEffect, useState } from 'react';
 import LevelBar from '@/components/LevelBar';
+import { explainLevel } from '@/utils/utils';
 
 export default function Profile() {
     const email = useEmail();
@@ -188,27 +189,6 @@ export default function Profile() {
         }
 
         alert('Saved!');
-    }
-
-    function explainLevel(level: number): string {
-        switch (level) {
-            case 0:
-                return '沒興趣';
-            case 1:
-                return '新手';
-            case 2:
-                return '休閒';
-            case 3:
-                return '中階';
-            case 4:
-                return '進階';
-            case 5:
-                return '半職業';
-            case 6:
-                return '職業';
-        }
-
-        return '';
     }
 
     return (
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,16 +1,20 @@
-import { redirect } from "next/navigation";
+export function explainLevel(level: number): string {
+    switch (level) {
+        case 0:
+            return '沒興趣';
+        case 1:
+            return '新手';
+        case 2:
+            return '休閒';
+        case 3:
+            return '中階';
+        case 4:
+            return '進階';
+        case 5:
+            return '半職業';
+        case 6:
+            return '職業';
+    }
 
-/**
- * Redirects to a specified path with an encoded message as a query parameter.
- * @param {('error' | 'success')} type - The type of message, either 'error' or 'success'.
- * @param {string} path - The path to redirect to.
- * @param {string} message - The message to be encoded and added as a query parameter.
- * @returns {never} This function doesn't return as it triggers a redirect.
- */
-export function encodedRedirect(
-  type: "error" | "success",
-  path: string,
-  message: string,
-) {
-  return redirect(`${path}?${type}=${encodeURIComponent(message)}`);
+    return '';
 }
