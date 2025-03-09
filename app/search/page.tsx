8e11fa69c0a782846ce24fd8ad9210a76e838a4f
Leo Lu <lyuholu@gmail.com>
2025-03-08 20:53:46 -0500

    feat: add start time filter

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,44 @@
+'use client';
+
+import { datetime2str } from '@/lib/utils';
+import { ChangeEvent, useEffect, useState } from 'react';
+
 export default function Search() {
-    return <></>;
+    const [startTime, setStartTime] = useState<Date | null>(null);
+
+    useEffect(() => {
+        setStartTimeNow();
+    }, []);
+
+    function handleStartTimeChange(e: ChangeEvent<HTMLInputElement>) {
+        setStartTime(new Date(e.target.value));
+    }
+
+    function setStartTimeNow() {
+        setStartTime(new Date());
+    }
+
+    return (
+        <>
+            <header>
+                <h1 className="text-center p-8 text-2xl font-bold">找運動</h1>
+            </header>
+            <main className="text-lg px-4">
+                <section className="flex justify-center gap-4">
+                    <input
+                        type="datetime-local"
+                        value={datetime2str(startTime)}
+                        onChange={handleStartTimeChange}
+                    />
+                    <button
+                        className="text-emerald-600 font-bold"
+                        onClick={setStartTimeNow}
+                    >
+                        馬上動！
+                    </button>
+                </section>
+                <section></section>
+            </main>
+        </>
+    );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -43,3 +43,21 @@ export function getSportEmoji(sport: string): string {
 
     return '';
 }
+
+export function datetime2str(datetime: Date | null): string {
+    if (!datetime) {
+        return '';
+    }
+
+    return (
+        datetime.getFullYear().toString() +
+        '-' +
+        (datetime.getMonth() + 1).toString().padStart(2, '0') +
+        '-' +
+        datetime.getDate().toString().padStart(2, '0') +
+        'T' +
+        datetime.getHours().toString().padStart(2, '0') +
+        ':' +
+        datetime.getMinutes().toString().padStart(2, '0')
+    );
+}
