d962038ae949134bed6cf02a0095437215a9f74c
Leo Lu <lyuholu@gmail.com>
2025-03-13 16:24:59 -0400

    fix: hide event card title that is too long

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -27,9 +27,12 @@ export default function EventCard({
             )}
             onClick={openCard}
         >
-            <div className="flex flex-col gap-4">
-                <div className="text-2xl">
-                    {getSportEmoji(sport)} {title}
+            <div className="flex flex-col gap-4 overflow-hidden">
+                <div className="text-2xl flex gap-4 w-full">
+                    <div>{getSportEmoji(sport)}</div>
+                    <div className="flex-grow text-ellipsis whitespace-nowrap overflow-hidden">
+                        {title}
+                    </div>
                 </div>
                 <div>
                     <span className="mr-4">地點:</span>
