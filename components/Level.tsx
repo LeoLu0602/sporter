187a0337c713939f7d4b601b513434bc09b5bea1
Leo Lu <lyuholu@gmail.com>
2025-03-13 18:11:27 -0400

    style: set level 0 to red

--- components/Level.tsx
+++ components/Level.tsx
@@ -13,7 +13,9 @@ export default function Level({
     return (
         <button
             className={clsx('border-[1px] w-8 h-8 rounded-full', {
-                'bg-emerald-500 border-emerald-500': level === index,
+                'bg-emerald-500 border-emerald-500':
+                    level === index && level > 0,
+                'bg-rose-500 border-rose-500': level === index && level === 0,
                 'bg-white border-[#aaa]': level !== index,
             })}
             onClick={handleClick}
