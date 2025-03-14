a69de2c02034d1967347e897670373c8ba701876
Leo Lu <lyuholu@gmail.com>
2025-03-14 15:58:28 -0400

    fix: prevent auto translation

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     return (
         <html
             lang="en"
+            translate="no"
             className={geistSans.className}
             suppressHydrationWarning
         >
