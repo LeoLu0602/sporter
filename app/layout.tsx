58030da2fd12df9f3788ca398f15ab7f9ed85912
Leo Lu <lyuholu@gmail.com>
2025-03-14 01:25:47 -0400

    fix: Sportify -> Sporter

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
     metadataBase: new URL(defaultUrl),
-    title: 'Sportify',
+    title: 'Sporter',
     description: '',
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Welcome() {
     return (
         <>
             <main className="fixed left-0 top-0 w-full h-screen flex flex-col justify-center items-center from-sky-500 bg-gradient-to-t text-white font-bold">
-                <h1 className="text-5xl mb-12">Sportify</h1>
+                <h1 className="text-5xl mb-12">Sporter</h1>
                 <ContinueWithGoogleBtn />
             </main>
         </>
