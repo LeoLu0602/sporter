de4c885d8bd10a47c3ee5f9d392b647dee88a970
Leo <96434243+LeoLu0602@users.noreply.github.com>
2025-03-23 02:15:23 -0400

    style: change welcome page color

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ContinueWithGoogleBtn from '@/components/ContinueWithGoogleBtn';
 export default function Welcome() {
     return (
         <>
-            <main className="fixed left-0 top-0 w-full h-screen flex flex-col justify-center items-center from-sky-500 bg-gradient-to-t text-white font-bold">
+            <main className="fixed left-0 top-0 w-full h-screen flex flex-col justify-center items-center from-sky-500 to-sky-200 bg-gradient-to-b text-white font-bold">
                 <h1 className="text-5xl mb-12">Sporter</h1>
                 <ContinueWithGoogleBtn />
             </main>
