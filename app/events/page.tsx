04e9056649a7ba62ad607d3841bdac07301f835f
Leo <96434243+LeoLu0602@users.noreply.github.com>
2025-03-23 01:03:24 -0400

    feat: add circular progress to the events page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -4,6 +4,7 @@ import EventCard from '@/components/EventCard';
 import EventDetails from '@/components/EventDetails';
 import { useUser, useUserEvents } from '@/context/Context';
 import { EventType } from '@/lib/utils';
+import { CircularProgress } from '@mui/material';
 import clsx from 'clsx';
 import { useState } from 'react';
 
@@ -12,10 +13,6 @@ export default function Event() {
     const userEvents = useUserEvents();
     const [eventDetails, setEventDetails] = useState<EventType | null>(null);
     const [option, setOption] = useState<number>(1);
-
-    if (!user || !userEvents) {
-        return <></>;
-    }
 
     function seeMoreDetails(id: string) {
         if (userEvents) {
@@ -29,17 +26,22 @@ export default function Event() {
         setEventDetails(null);
     }
 
-    const userEvents1 = userEvents.filter(
-        ({ remaining_spots, start_time }) =>
-            new Date(start_time).getTime() > Date.now() && remaining_spots > 0
-    );
-    const userEvents2 = userEvents.filter(
-        ({ remaining_spots, start_time }) =>
-            new Date(start_time).getTime() > Date.now() && remaining_spots === 0
-    );
-    const userEvents3 = userEvents.filter(
-        ({ start_time }) => new Date(start_time).getTime() <= Date.now()
-    );
+    const userEvents1 =
+        userEvents?.filter(
+            ({ remaining_spots, start_time }) =>
+                new Date(start_time).getTime() > Date.now() &&
+                remaining_spots > 0
+        ) ?? [];
+    const userEvents2 =
+        userEvents?.filter(
+            ({ remaining_spots, start_time }) =>
+                new Date(start_time).getTime() > Date.now() &&
+                remaining_spots === 0
+        ) ?? [];
+    const userEvents3 =
+        userEvents?.filter(
+            ({ start_time }) => new Date(start_time).getTime() <= Date.now()
+        ) ?? [];
     const shownUserEvents =
         option === 1 ? userEvents1 : option === 2 ? userEvents2 : userEvents3;
 
@@ -47,86 +49,74 @@ export default function Event() {
         <>
             <nav className="py-4 text-xl sticky left-0 top-0 bg-white border-b-[1px] border-gray-200">
                 <ul className="flex w-full justify-around">
-                    <li
-                        className={clsx('cursor-pointer', {
-                            'border-b-2 border-b-black text-black':
-                                option === 1,
-                            'text-gray-500': option !== 1,
-                        })}
-                        onClick={() => {
-                            setOption(1);
-                        }}
-                    >
-                        未滿
-                    </li>
-                    <li
-                        className={clsx('cursor-pointer', {
-                            'border-b-2 border-b-black text-black':
-                                option === 2,
-                            'text-gray-500': option !== 2,
-                        })}
-                        onClick={() => {
-                            setOption(2);
-                        }}
-                    >
-                        已滿
-                    </li>
-                    <li
-                        className={clsx('cursor-pointer', {
-                            'border-b-2 border-b-black text-black':
-                                option === 3,
-                            'text-gray-500': option !== 3,
-                        })}
-                        onClick={() => {
-                            setOption(3);
-                        }}
-                    >
-                        歷史
-                    </li>
+                    {['未滿', '已滿', '歷史'].map((txt, i) => (
+                        <li
+                            key={i}
+                            className={clsx('cursor-pointer', {
+                                'border-b-2 border-b-black text-black':
+                                    option === i + 1,
+                                'text-gray-500': option !== i + 1,
+                            })}
+                            onClick={() => {
+                                setOption(i + 1);
+                            }}
+                        >
+                            {txt}
+                        </li>
+                    ))}
                 </ul>
             </nav>
             <main>
-                {eventDetails && (
-                    <EventDetails
-                        userEmail={user.email}
-                        details={eventDetails}
-                        hideDetails={() => {
-                            hideDetails();
-                        }}
-                    />
+                {user ? (
+                    <>
+                        {' '}
+                        {eventDetails && (
+                            <EventDetails
+                                userEmail={user.email}
+                                details={eventDetails}
+                                hideDetails={() => {
+                                    hideDetails();
+                                }}
+                            />
+                        )}
+                        <section className="pb-12">
+                            {shownUserEvents.length > 0 ? (
+                                shownUserEvents.map(
+                                    ({
+                                        id,
+                                        email,
+                                        sport,
+                                        title,
+                                        start_time,
+                                        end_time,
+                                        location,
+                                    }) => (
+                                        <EventCard
+                                            key={id}
+                                            isOwner={user.email === email}
+                                            sport={sport}
+                                            title={title}
+                                            startTime={new Date(start_time)}
+                                            endTime={new Date(end_time)}
+                                            location={location}
+                                            openCard={() => {
+                                                seeMoreDetails(id);
+                                            }}
+                                        />
+                                    )
+                                )
+                            ) : (
+                                <div className="text-center py-8 text-xl text-gray-500">
+                                    Empty
+                                </div>
+                            )}
+                        </section>
+                    </>
+                ) : (
+                    <div className="flex justify-center mt-8">
+                        <CircularProgress />
+                    </div>
                 )}
-                <section className="pb-12">
-                    {shownUserEvents.length > 0 ? (
-                        shownUserEvents.map(
-                            ({
-                                id,
-                                email,
-                                sport,
-                                title,
-                                start_time,
-                                end_time,
-                                location,
-                            }) => (
-                                <EventCard
-                                    key={id}
-                                    isOwner={user.email === email}
-                                    sport={sport}
-                                    title={title}
-                                    startTime={new Date(start_time)}
-                                    endTime={new Date(end_time)}
-                                    location={location}
-                                    openCard={() => {
-                                        seeMoreDetails(id);
-                                    }}
-                                />
-                            )
-                        )
-                    ) : (
-                        <div className="text-center py-8 text-xl text-gray-500">
-                            空空如也~
-                        </div>
-                    )}
-                </section>
             </main>
         </>
     );
