99037fde80699378d87de27accea168bad4fca0c
Leo <96434243+LeoLu0602@users.noreply.github.com>
2025-04-05 18:54:38 -0400

    feat: add logo #19

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,13 @@ import ContinueWithGoogleBtn from '@/components/ContinueWithGoogleBtn';
 export default function Welcome() {
     return (
         <>
-            <main className="fixed left-0 top-0 w-full h-screen flex flex-col justify-center items-center from-sky-500 to-sky-200 bg-gradient-to-b text-white font-bold">
-                <h1 className="text-5xl mb-12">Sporter</h1>
+            <main className="fixed left-0 top-0 w-full h-screen flex flex-col justify-center items-center font-bold">
+                <img
+                    className="w-40 h-40 mb-12"
+                    src="/logo.png"
+                    alt="Sporter Logo"
+                />
+                <h1 className="text-5xl mb-12 text-[#499c2c]">Sporter</h1>
                 <ContinueWithGoogleBtn />
             </main>
         </>
