efb4ce6cc5328abef5e621900b88e2bae89bcbb3
Leo <96434243+LeoLu0602@users.noreply.github.com>
2025-04-06 19:26:01 -0400

    fix: update logo #19

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,7 @@ export default function Welcome() {
     return (
         <>
             <main className="fixed left-0 top-0 w-full h-screen flex flex-col justify-center items-center font-bold">
-                <img
-                    className="w-40 h-40 mb-12"
-                    src="/logo.png"
-                    alt=""
-                />
-                <h1 className="text-5xl mb-12 text-[#499c2c]">Sporter</h1>
+                <img className="w-4/5 mb-12" src="/logo-2.png" alt="" />
                 <ContinueWithGoogleBtn />
             </main>
         </>
